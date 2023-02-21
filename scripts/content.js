0e953c6a2393c02838023f03277c2cd8457b9a06
zalwicker2 <zalwicker@gmail.com>
2023-02-21 16:25:01 -0600

    change stuff

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,25 @@
 function RunCode(event) {
+    //console.log(event.relatedNode.nodeName + " " + event.relatedNode.matches('ytd-thumbnail-overlay-time-status-renderer[overlay-style="UPCOMING"]'))
+    /*if(event.relatedNode.matches('ytd-grid-video-renderer')) {
+        console.log(event.relatedNode.querySelector('a').href.match(/\/shorts\//));
+    }*/
     let shittyVideoIcons = document.querySelectorAll('ytd-thumbnail-overlay-time-status-renderer[overlay-style="UPCOMING"], ytd-thumbnail-overlay-time-status-renderer[overlay-style="SHORTS"]');
     shittyVideoIcons.forEach(item => {
-        item.closest('ytd-grid-video-renderer').remove();
+        const parentToRemove = item.closest('ytd-grid-video-renderer')
+        if(parentToRemove != null) {
+            parentToRemove.remove();
+        }
+    })
+    let shittyVideoSection = document.querySelectorAll('.yt-simple-endpoint[title="Shorts"]')
+    shittyVideoSection.forEach(item => {
+        item.parentElement.remove();
+    })
+    let shittyMixSection = document.querySelectorAll('a[href$="&start_radio=1"]')
+    shittyMixSection.forEach(item => {
+        const parentToRemove = item.closest('ytd-rich-item-renderer');
+        if(parentToRemove != null) {
+            parentToRemove.remove();
+        }
     })
 };
 
